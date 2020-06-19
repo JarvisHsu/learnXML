37c35c6791010bf4f9d6619c6a9cb7f5222bd2a9
JarvisHsu <xu3256622923@gmail.com>
2020-06-19 11:56:22 +0800

    update dom4jTest

--- StuInfoManagement/src/cn/itcast/service/StuService.java
+++ StuInfoManagement/src/cn/itcast/service/StuService.java
@@ -4,10 +4,14 @@ import cn.itcast.utils.Dom4jUtils;
 import cn.itcast.vo.Student;
 import org.dom4j.Document;
 import org.dom4j.Element;
+import org.dom4j.Node;
+
+import java.util.List;
 
 
 public class StuService {
     /**
+     * 添加一条学生信息
      * * 1、创建解析器
      * * 2、得到document
      * * 3、获取root节点
@@ -36,5 +40,80 @@ public class StuService {
         stu_age.setText(student.getAge());
         //7
         Dom4jUtils.xmlWriters(document, Dom4jUtils.PATH);
+    }
+    /**
+     * 删除一条学生信息
+     * * 1、创建解析器
+     * * 2、得到document
+     * * 3、获取到所有id
+     * *    使用xpath //id 返回List<Node>集合
+     * * 4、遍历集合List 进行id比较
+     * * 5、删除找到的id值
+     * *
+     * * 6、回写xml
+     *
+     * @param id 根据id进行删除
+     */
+    public static void delStu(String id){
+        int count = 0;
+        //1 2
+        Document document = Dom4jUtils.getDocument(Dom4jUtils.PATH);
+        //3
+        List<Node> list_id = document.selectNodes("//id");
+        //4
+        for (Node node : list_id) {
+            //5
+            if (id.equals(node.getText())) {
+                //得到id的父节点stu
+                Element stu = node.getParent();
+                //得到student节点
+                Element student =stu.getParent();
+                student.remove(stu);
+                count++;
+                //只删除找到的第一条匹配信息
+                break;
+            }
+        }
+        //不为0则找到id
+        if (count!=0){
+            //6
+            Dom4jUtils.xmlWriters(document,Dom4jUtils.PATH);
+            System.out.println("共删除"+count+"条id为"+id+"的数据");
+        }else {
+            System.out.println("未找到符合信息");
+        }
+    }
+    /**
+     * 1、创建解析器
+     * 2、得到document
+     * 3、获取到所有id
+     *     使用xpath //id 返回List<Node>集合
+     * 4、遍历集合List 进行id比较
+     * 5、分别设置 id name age 属性值
+     * @param id 根据id查询学生信息
+     * @return  返回查询到的student信息
+     */
+    public static Student getStu(String id){
+        Student student = null;
+        //1 2
+        Document document = Dom4jUtils.getDocument(Dom4jUtils.PATH);
+        //3
+        List<Node> list_id = document.selectNodes("//id");
+        //4
+        for (Node node : list_id) {
+            if (id.equals(node.getText())) {
+                //得到stu节点
+                Element stu = node.getParent();
+                Element stu_id = stu.element("id");
+                Element stu_name = stu.element("name");
+                Element stu_age = stu.element("age");
+                //5
+                student = new Student();
+                student.setId(stu_id.getText());
+                student.setName(stu_name.getText());
+                student.setAge(stu_age.getText());
+            }
+        }
+        return student;
     }
 }
--- StuInfoManagement/src/cn/itcast/test/TestStu.java
+++ StuInfoManagement/src/cn/itcast/test/TestStu.java
@@ -5,7 +5,7 @@ import cn.itcast.vo.Student;
 
 public class TestStu {
     public static void main(String[] args) {
-        testAdd();
+        testSel();
     }
 
     //测试添加方法
@@ -16,5 +16,18 @@ public class TestStu {
         stu.setName("wangwu");
         stu.setAge("40");
         StuService.addStu(stu);
+    }
+    //测试删除方法
+    private static void testDel(){
+        //设置值
+        String id = "103";
+        StuService.delStu(id);
+    }
+    //测试查询方法
+    private static void testSel(){
+        //设置值
+        String id = "100";
+        Student stu = StuService.getStu(id);
+        System.out.println(stu);
     }
 }
